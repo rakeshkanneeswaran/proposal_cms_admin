a578a14e42aa3f833787ad28b2f95f3ec6e2f227
Rakesh Kanneeswaran <rm1404@srmist.edu.in>
2024-05-06 16:29:30 +0530

    added delete endpoint

--- app/api/proposal/route.ts
+++ app/api/proposal/route.ts
@@ -1,9 +1,35 @@
-
-
 import { NextRequest, NextResponse } from 'next/server';
 import prisma from '@/database';
 import { getServerSession } from "next-auth"
 
+
+
+//for getting all proposals
+export async function GET() {
+    const session = await getServerSession();
+    if (session?.user?.name) {
+        console.log(session?.user?.name)
+        try {
+            const existingUser = await prisma.admin.findFirst({
+                where: {
+                    username: session?.user?.name
+                }
+            });
+            if (existingUser) {
+                const allproposal = await prisma.proposal.findMany()
+                console.log("printing all proposals")
+                console.log(allproposal)
+                return NextResponse.json({ proposal: allproposal })
+
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    console.log(session?.user?.name)
+}
+
+//for addding a proposal
 
 export async function POST(req: NextRequest) {
     const body = await req.json();
@@ -29,40 +55,45 @@ export async function POST(req: NextRequest) {
                         financialSupportOthers: body.financialSupportOthers,
                         financialSupportSRMIST: body.financialSupportSRMIST,
                         estimatedBudget: body.estimatedBudget,
-
                     }
                 })
                 console.log("added data as follows")
                 console.log(eventadded)
+                return NextResponse.json({ messgae: "Added event successfully" })
             }
         } catch (error) {
             console.log(error)
         }
     }
-    return NextResponse.json({ messgae: "Added event successfully" })
+    return NextResponse.json({ messgae: "Validation error" })
 }
 
 
-export async function GET(req: NextRequest) {
+
+// FOR deleting the a proposal
+export async function DELETE(request: NextRequest) {
     const session = await getServerSession();
     if (session?.user?.name) {
-        console.log(session?.user?.name)
         try {
+            const searchParams = request.nextUrl.searchParams;
+            const id = searchParams.get('id');
+            if (id == null) {
+                return NextResponse.json({ "messgae": "could not find the proposal" })
+            }
             const existingUser = await prisma.admin.findFirst({
                 where: {
                     username: session?.user?.name
                 }
             });
             if (existingUser) {
-                const allproposal = await prisma.proposal.findMany()
-                console.log("printing all proposals")
-                console.log(allproposal)
-                return NextResponse.json({ proposal: allproposal })
-
+                const result = await prisma.proposal.delete({
+                    where: { id: parseInt(id) }
+                })
+                console.log(result)
+                return NextResponse.json({ messgae: "Delete event successfully" })
             }
         } catch (error) {
             console.log(error)
         }
     }
-    console.log(session?.user?.name)
 }
