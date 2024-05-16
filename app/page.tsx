74be40a1f921e98115b73660d00d61507d3b160b
Rakesh Kanneeswaran <rm1404@srmist.edu.in>
2024-05-16 23:11:23 +0530

    changed root page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
 import { getServerSession } from "next-auth";
 import { redirect } from 'next/navigation'
-import authOptions from "@/lib/auth";
 
 export default async function Page() {
-  const session = await getServerSession(authOptions);
+  const session = await getServerSession();
   if (session?.user) {
     redirect('/dashboard')
   } else {
-    redirect('/api/auth/signin')
+    redirect('/signin')
   }
   
 }
