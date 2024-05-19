80b621fb0bdcb0035b9e97e20ca4939380a9a850
Rakesh Kanneeswaran <rm1404@srmist.edu.in>
2024-05-19 06:52:40 +0530

    made some ui changes

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -68,7 +68,7 @@ export default function LoginForm() {
                 Department of Computing Technologies
               </span>{' '}
               <span > In-House Proposal Management</span>
-              <p className='text-sm font-normal pt-3 '>Discover a powerful tool for proposal management created by the Department of Computing Technologies (CTech) in collaboration with talented students from the School of Computing at SRM University. This innovative platform, known as SRM Event Connect, simplifies and enhances the proposal management process with its intuitive design and advanced features. Ideal for organizing events, projects, and initiatives, SRM Event Connect streamlines proposal creation, submission, and review.</p>
+              <p className='text-xl font-normal  pt-3 '>Discover a powerful tool for proposal management created by the Department of Computing Technologies (CTech) in collaboration with talented students from the School of Computing at SRM University. This innovative platform, known as SRM Event Connect, simplifies and enhances the proposal management process with its intuitive design and advanced features. Ideal for organizing events, projects, and initiatives, SRM Event Connect streamlines proposal creation, submission, and review.</p>
 
             </h1>
 
