6584bf6cd616c49e5cfa742df94b415bfbd00845
Rakesh Kanneeswaran <rm1404@srmist.edu.in>
2024-06-07 17:08:35 +0530

    test

--- components/appbardashboard.tsx
+++ components/appbardashboard.tsx
@@ -54,6 +54,7 @@ export const Appbardashboard = ({ onClick, children }: any) => {
               className="text-white bg-blue-700 h-14  hover:bg-red-600   border border-black focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2"
               onClick={async () => {
                 const result = await axios.get("/api/sendnotification")
+                console.log(result)
                 if (result.status == 200) {
                   alert("Notification sent successfully")
                 }
