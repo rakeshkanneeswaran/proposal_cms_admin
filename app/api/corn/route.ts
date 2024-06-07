55963d3e185fd83c4a15ceafdcabc7edb5d13d81
Rakesh Kanneeswaran <rm1404@srmist.edu.in>
2024-06-07 13:47:58 +0530

    added corn job

---
+++ app/api/corn/route.ts
@@ -0,0 +1,10 @@
+import { NextRequest, NextResponse } from 'next/server';
+import prisma from '@/database';
+
+import emailSender from '@/emails';
+
+export async function GET(request: NextRequest) {
+
+    emailSender({ receiverEmail: "rakikanneeswaran", subject: "this is corn job ", text: " this is corn job text " })
+    return NextResponse.json({ "message": "message send successfully" });
+}
