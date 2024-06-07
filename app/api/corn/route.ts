aa9feb566b7034e52116ae4de926684f0baeecb4
Rakesh Kanneeswaran <rm1404@srmist.edu.in>
2024-06-07 13:55:23 +0530

    test

--- app/api/corn/route.ts
+++ app/api/corn/route.ts
@@ -1,7 +1,42 @@
 import { NextRequest, NextResponse } from 'next/server';
-import prisma from '@/database';
 
-import emailSender from '@/emails';
+import nodemailer from "nodemailer";
+
+const transporter = nodemailer.createTransport({
+    port: 465,
+    host: "smtp.gmail.com",
+    auth: {
+        user: process.env.NDOEMAILER_USERNAME,
+        pass: process.env.NDOEMAILER_PASSWORD
+    },
+    secure: true,
+    tls: { rejectUnauthorized: false }
+});
+
+export default async function emailSender({ receiverEmail, subject, text }: { receiverEmail: string, subject: string, text: string }) {
+    const mailOptions = {
+        from: "ctecheventconnect@gmail.com",
+        to: receiverEmail,
+        subject: subject,
+        text: text,
+        html: `<p>${text}</p>`
+    };
+
+    try {
+        await transporter.verify();
+        console.log("Server is ready to take our messages"); // Debug line
+        const info = await transporter.sendMail(mailOptions);
+        console.log("Email sent:", info); // Debug line
+        return {
+            status: "Success!",
+            info: info
+        };
+    } catch (error) {
+        console.error("Email sending error:", error); // Detailed error logging
+        throw new Error("Failed to send email");
+    }
+}
+
 
 export async function GET(request: NextRequest) {
 
