15011ee938a0574b2a4c9579c9d15d0057109a0a
Rakesh Kanneeswaran <rm1404@srmist.edu.in>
2024-07-16 14:06:51 +0530

    Checkbox removed

--- components/Table.tsx
+++ components/Table.tsx
@@ -74,28 +74,7 @@ export default function EventTable() {
   }, []);
 
   const eventColumns: ColumnDef<Event>[] = [
-    {
-      id: "select",
-      header: ({ table }) => (
-        <Checkbox
-          checked={
-            table.getIsAllPageRowsSelected() ||
-            (table.getIsSomePageRowsSelected() && "indeterminate")
-          }
-          onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-          aria-label="Select all"
-        />
-      ),
-      cell: ({ row }) => (
-        <Checkbox
-          checked={row.getIsSelected()}
-          onCheckedChange={(value) => row.toggleSelected(!!value)}
-          aria-label="Select row"
-        />
-      ),
-      enableSorting: false,
-      enableHiding: false,
-    },
+    
     {
       accessorKey: "eventTitle",
       header: "Event Title",
